a1e92e9c295c9bc942a0c4c00964de3e367dafd4
Paul <paul@paul-desktop-k>
2018-09-23 22:32:09 +0100

    Added echos, removed fluff

--- setup.sh
+++ setup.sh
@@ -7,20 +7,20 @@ sudo apt install -y tmux
 # General dev tools: git, vim
 sudo apt install -y git vim
 
-# Rust dev
-
-
 # Require other tools: ranger (file manager), cmus (music player), w3m (web browser)
 sudo apt install -y ranger cmus w3m
-
 
 # ---Configure---
 # Install Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # Copy .vimrc
+echo "Copying .vimrc"
 cp .vimrc ~/.vimrc
+echo "Done"
 
 # Copy tmux.conf
+echo "Copying .tmux.conf"
 cp .tmux.conf ~/.tmux.conf
+echo "Done"
 
