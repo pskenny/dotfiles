5bcdb024608140b3044bf59f2202792ec539fa3d
Paul Kenny <paul.s.kenny@gmail.com>
2018-09-24 11:35:33 +0100

    Updated with clang-format, others

--- .vimrc
+++ .vimrc
@@ -17,11 +17,28 @@ Plugin 'itchyny/lightline.vim'
 Plugin 'scrooloose/nerdtree'
 " Git gutter
 Plugin 'airblade/vim-gitgutter'
+" Color schemes
+Plugin 'flazz/vim-colorschemes'
 
-" " All of your Plugins must be added before the following line
-call vundle#end()            " required
+" Add maktaba and codefmt to the runtimepath.
+" " (The latter must be installed before it can be used.)
+Plugin 'google/vim-maktaba'
+Plugin 'google/vim-codefmt'
+" " Also add Glaive, which is used to configure codefmt's maktaba flags. See
+" " `:help :Glaive` for usage.
+Plugin 'google/vim-glaive'
+" " ...
+call vundle#end()
+" the glaive#Install() should go after the "call vundle#end()"
+call glaive#Install()
+" Optional: Enable codefmt's default mappings on the <Leader>= prefix.
+Glaive codefmt plugin[mappings]
+Glaive codefmt google_java_executable="java -jar /path/to/google-java-format-VERSION-all-deps.jar"
+
 filetype plugin indent on    " required
 
 " Open NERDTree when vim starts
 autocmd vimenter * NERDTree
+
+colorscheme moonshine
 
--- setup.sh
+++ setup.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
-printf "Updating and upgrading system...\n"
-sudo apt update && sudo apt upgrade
+while true; do
+	read -p "Update and upgrade apt?" yn
+	        case $yn in
+			[Yy]* ) sudo apt update && sudo apt upgrade; break;;
+			[Nn]* ) break;;
+			    * ) echo "Please answer yes or no.";;
+	esac
+done
 
 # ---Installs---
 # Essential
-sudo apt install -y tmux
+printf "\n\nInstalling tmux cowsay"
+sudo apt install -y tmux cowsay
 
-# General dev tools: git, vim
-sudo apt install -y git vim
+# Dev tools: git, vim
+printf "\n\nInstalling git vim clang-format (for vim code formatting)"
+sudo apt install -y git vim clang-format
 
-# Require other tools: ranger (file manager), cmus (music player), w3m (web browser)
+# General: ranger (file manager), cmus (music player), w3m (web browser)
+printf "\n\nInstalling ranger cmus w3m\n"
 sudo apt install -y ranger cmus w3m
 
 # ---Configure---
@@ -19,12 +28,14 @@ printf "\nInstalling Vundle\n"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # Copy .vimrc
-printf "\nCopying .vimrc...\n"
+printf "\nCopying .vimrc..."
 cp .vimrc ~/.vimrc
-echo "Done"
+printf "\nDone"
 
 # Copy tmux.conf
-printf "\nCopying .tmux.conf...\n"
+printf "\nCopying .tmux.conf..."
 cp .tmux.conf ~/.tmux.conf
-echo "Done"
+printf "\nDone\n"
+
+cowsay "Make sure to run :PluginInstall in vim!"
 
