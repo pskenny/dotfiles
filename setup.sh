eb8598f172496197752dd00708b96233ce015ee8
Paul <paul@paul-desktop-k>
2018-09-30 21:36:10 +0100

    Changed output text

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while true; do
-	read -p "Update and upgrade apt?" yn
+	read -p "Update and upgrade apt (y/n)? " yn
 	        case $yn in
 			[Yy]* ) sudo apt update && sudo apt upgrade; break;;
 			[Nn]* ) break;;
@@ -15,7 +15,7 @@ printf "\n\nInstalling tmux cowsay"
 sudo apt install -y tmux cowsay
 
 # Dev tools: git, vim
-printf "\n\nInstalling git vim clang-format (for vim code formatting)"
+printf "\n\nInstalling git vim clang format (for vim code formatting)"
 sudo apt install -y git vim clang-format
 
 # General: ranger (file manager), cmus (music player), w3m (web browser)
@@ -25,7 +25,11 @@ sudo apt install -y ranger cmus w3m
 # ---Configure---
 # Install Vundle
 printf "\nInstalling Vundle\n"
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+if git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim ; then
+    echo "Done"
+else
+    echo "Couldn't clone, Vundle already present"
+fi
 
 # Copy .vimrc
 printf "\nCopying .vimrc..."
@@ -37,5 +41,5 @@ printf "\nCopying .tmux.conf..."
 cp .tmux.conf ~/.tmux.conf
 printf "\nDone\n"
 
-cowsay "Make sure to run :PluginInstall in vim!"
+cowsay -f /usr/share/cowsay/cows/dragon.cow "Make sure to run :PluginInstall in vim!"
 
