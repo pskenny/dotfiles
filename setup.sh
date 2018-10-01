e7171ff9840e618374aa86147782bef101128a54
Paul <paul@paul-desktop-k>
2018-10-01 19:09:34 +0100

    Updated

--- setup.sh
+++ setup.sh
@@ -1,45 +1,55 @@
 #!/bin/bash
 
+ESSENTIAL="tmux cowsay"
+GENERAL="ranger cmus w3m"
+DEV="vim clang-format"
+
+function dev_setup() {
+	# Install Vundle
+	printf "\nInstalling Vundle\n"
+	if git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim ; then
+		echo "Done"
+	else
+		echo "Couldn't clone Vundle"
+	fi
+
+	# Copy .vimrc
+	printf "\nCopying .vimrc..."
+	cp .vimrc ~/.vimrc
+	printf "\nDone"
+
+	# Copy tmux.conf
+	printf "\nCopying .tmux.conf..."
+	cp .tmux.conf ~/.tmux.conf
+	printf "\nDone\n"
+
+
+	cowsay -f /usr/share/cowsay/cows/dragon.cow "Make sure to run :PluginInstall in vim!"
+}
+
 while true; do
-	read -p "Update and upgrade apt (y/n)? " yn
-	        case $yn in
-			[Yy]* ) sudo apt update && sudo apt upgrade; break;;
-			[Nn]* ) break;;
-			    * ) echo "Please answer yes or no.";;
+	read -p "Update, upgrade and clean (y/n)? " yn
+	case $yn in
+		[Yy]* ) sudo apt update && sudo apt upgrade && sudo apt autoremove -y; break;;
+		[Nn]* ) break;;
+		* ) echo "Please answer yes or no.";;
 	esac
 done
 
-# ---Installs---
-# Essential
-printf "\n\nInstalling tmux cowsay"
-sudo apt install -y tmux cowsay
-
-# Dev tools: git, vim
-printf "\n\nInstalling git vim clang format (for vim code formatting)"
-sudo apt install -y git vim clang-format
-
-# General: ranger (file manager), cmus (music player), w3m (web browser)
-printf "\n\nInstalling ranger cmus w3m\n"
-sudo apt install -y ranger cmus w3m
-
-# ---Configure---
-# Install Vundle
-printf "\nInstalling Vundle\n"
-if git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim ; then
-    echo "Done"
-else
-    echo "Couldn't clone, Vundle already present"
-fi
-
-# Copy .vimrc
-printf "\nCopying .vimrc..."
-cp .vimrc ~/.vimrc
-printf "\nDone"
-
-# Copy tmux.conf
-printf "\nCopying .tmux.conf..."
-cp .tmux.conf ~/.tmux.conf
-printf "\nDone\n"
-
-cowsay -f /usr/share/cowsay/cows/dragon.cow "Make sure to run :PluginInstall in vim!"
+while true; do
+	echo
+	echo "Install:"
+	echo "   1) Essential"
+	echo "   2) Essential + general"
+	echo "   3) Essential + general + dev"
+	echo "   4) Cancel"	
+	read -p "Enter option: " option
+	case $option in
+		[1]* ) sudo apt install -y $ESSENTIAL; break;;
+		[2]* ) sudo apt install -y $ESSENTIAL $GENERAL; break;;
+		[3]* ) sudo apt install -y $ESSENTIAL $GENERAL $DEV; dev_setup;  break;;
+		[4]* ) break;;
+		   * ) echo "Feed me numbers ^_^";;
+	esac
+done
 
