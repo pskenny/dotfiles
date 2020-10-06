772425b0d0e15ddc931823b3043b655154af484b
Paul Kenny <paul.s.kenny@gmail.com>
2020-10-06 03:06:37 +0100

    Add fish greeting

--- config.fish
+++ config.fish
@@ -3,3 +3,13 @@ set PATH $PATH ~/.local/bin
 
 # Add Cargo bin to path
 set PATH $PATH ~/.cargo/bin
+
+function fish_greeting
+	set todo ~/Dropbox/todo.md
+	if test -e "$todo" 
+		cat $todo | cowsay
+	end
+end
+
+funcsave fish_greeting
+
